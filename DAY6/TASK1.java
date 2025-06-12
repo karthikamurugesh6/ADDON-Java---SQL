404734dace839aea595e1e65e8ee1b0058904bc4
karthikamurugesh6 <163977258+karthikamurugesh6@users.noreply.github.com>
2025-06-12 10:12:52 +0530

    Update TASK1.java

--- DAY6/TASK1.java
+++ DAY6/TASK1.java
@@ -1,46 +1,17 @@
 package method;
 import java.util.ArrayList;
-import java.util.Iterator;
-
-class Student {
-    private String name;
-    private int rollNumber;
-
-    public  Student(String name, int rollNumber) {
-        this.name = name;
-        this.rollNumber = rollNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getRollNumber() {
-        return rollNumber;
-    }
-
-    public String toString() {
-        return "Name: " + name + ", Roll Number: " + rollNumber;
-    }
+public class collection {
+            public static void main(String[]args) {
+            	ArrayList<Object> arr=new ArrayList<Object>();
+            	arr.add("A");
+            	arr.add("cs");
+            	arr.add("3");
+            	arr.add("hari");
+            	arr.add("priya");
+       System.out.println(arr.get(1));
+       //for(type iteration;existing variable)
+       for(Object data:arr) {
+    	   System.out.println(data);
+       }
+            }
 }
-
-public class  Array {
-    public static void main(String[] args) {
-       
-        ArrayList<Student> students = new ArrayList<>();
-        students.add(new Student("abc", 22));
-        students.add(new Student("def", 23));
-        students.add(new Student("ghi", 24));
-        students.add(new Student("jkl", 25));
-
-       
-        Iterator<Student> iterator = students.iterator();
-
-        
-        System.out.println("Student List:");
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-    }
-}
-
