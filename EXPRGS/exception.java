36f73ad74a833b9de8b9279df41add7fa86b6dd1
karthikamurugesh6 <163977258+karthikamurugesh6@users.noreply.github.com>
2025-06-13 11:40:45 +0530

    Create exception.java

---
+++ EXPRGS/exception.java
@@ -0,0 +1,14 @@
+package method;
+public class GradeEvaluation {
+    public static void main(String[] args) {
+		try {
+			int i=(Integer) null;
+			System.out.println(i);
+			}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+
+}
